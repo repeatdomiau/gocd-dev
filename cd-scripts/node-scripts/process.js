492fdfd22c80a6604e1ba58ff317e390ea9fd3a1
repeatdomiau <gabriel.militello@gmail.com>
2020-11-22 06:18:15 -0300

    teste

--- cd-scripts/node-scripts/process.js
+++ cd-scripts/node-scripts/process.js
@@ -2,8 +2,6 @@ const fs = require('fs');
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-
-const changesFile = process.argv[2];
 const trace = x => { console.log(x); return x; }
 const toString = obj => obj.toString();
 const split = sep => text => text.split(sep);
@@ -13,6 +11,7 @@ const byTruthy = x => x;
 const arrayToSet = arr => new Set(arr);
 const setToArray = set => [...set];
 
+const changesFile = process.argv[2];
 const db_host = process.env.MONGODB_HOST_NAME;
 const db_user = process.env.MONGODB_USER;
 const db_pass = process.env.MONGODB_PASSWORD;
@@ -27,15 +26,6 @@ if (changesFile) {
         .then(toString)
         .then(splitLines)
         .then(filter(byTruthy))
-        .then(arrayToSet)
-        .then(setToArray)
-        .then(files => {
-
-            const db = mongoose.createConnection(`mongodb://${db_user}:${db_pass}@${db_host}:${db_port}`, { useUnifiedTopology: true, useNewUrlParser: true });
-            db.once('open', () => {
-                db.useDb(db_base);
-                db.close();
-            })
-
-        }).catch(console.error);
+        .then(trace)
+        .catch(console.error);
 }

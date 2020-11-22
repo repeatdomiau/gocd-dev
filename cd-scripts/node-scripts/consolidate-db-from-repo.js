dbe867165d11038b90473f6a9783275c65dae77c
repeatdomiau <gabriel.militello@gmail.com>
2020-11-22 06:27:50 -0300

    x

--- cd-scripts/node-scripts/consolidate-db-from-repo.js
+++ cd-scripts/node-scripts/consolidate-db-from-repo.js
@@ -11,21 +11,27 @@ const byTruthy = x => x;
 const arrayToSet = arr => new Set(arr);
 const setToArray = set => [...set];
 
-const changesFile = process.argv[2];
+
 const db_host = process.env.MONGODB_HOST_NAME;
 const db_user = process.env.MONGODB_USER;
 const db_pass = process.env.MONGODB_PASSWORD;
 const db_base = process.env.MONGODB_DATABASE;
 const db_port = process.env.MONGODB_PORT;
 
+const run = async () => {
+    const changesFile = process.argv[2];
 
-if (changesFile) {
-    require('fs')
-        .promises
-        .readFile(changesFile)
-        .then(toString)
-        .then(splitLines)
-        .then(filter(byTruthy))
-        .then(trace)
-        .catch(console.error);
+    if (changesFile) {
+        await require('fs')
+            .promises
+            .readFile(changesFile)
+            .then(toString)
+            .then(splitLines)
+            .then(filter(byTruthy))
+            .then(trace)
+            .catch(console.error);
+    }
+
 }
+
+run();
